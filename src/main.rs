5bf5f62c61af698b327d8d3e0a4516dd3dbdbe19
Johnny <jsklucinec@gmail.com>
2024-11-12 12:59:39 -0500

    Added skeleton for main menu.
    Main menu appears by default.
    Pressing "G" hides the menu.
    Pressing "M" brings back the menu.
    Pressing "Escape" closes the simulation.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,33 @@
 use bevy::prelude::*;
+mod game;
+mod main_menu;
+mod systems;
+
+use game::GamePlugin;
+use main_menu::MainMenuPlugin;
+use systems::*;
 
 fn main() {
     App::new()
+        // Bevy Plugins
         .add_plugins(DefaultPlugins)
+        .init_state::<AppState>()
+        // Game Plugins
+        .add_plugins(MainMenuPlugin)
+        .add_plugins(GamePlugin)
+        // Game Systems
         .insert_resource(ClearColor(Color::srgb(0.2, 0.2, 0.2)))
         .add_systems(Startup, setup)
+        .add_systems(Update, transition_to_game_state)
+        .add_systems(Update, transition_to_main_menu_state)
+        .add_systems(Update, exit_game)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // Camera
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(0.0, 5.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
-    ));
-
-    // circular base
-    commands.spawn((
-        Mesh3d(meshes.add(Circle::new(4.0))),
-        MeshMaterial3d(materials.add(Color::WHITE)),
-        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
-    ));
-    // cube
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
-        Transform::from_xyz(0.0, 0.5, 0.0),
-    ));
-
-    // Light
-    commands.spawn((
-        PointLight {
-            intensity: 3500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        Transform::from_xyz(4.0, 8.0, 4.0),
-    ));
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum AppState {
+    #[default]
+    MainMenu,
+    Game,
+    GameOver,
 }
