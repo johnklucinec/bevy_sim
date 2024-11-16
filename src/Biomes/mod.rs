fd976e4f25c01a7b8de0930b5eee85b95e0b403e
brant cass <brantcass7@gmail.com>
2024-11-15 21:30:16 -0600

    Added perlin noise to cargo.toml, and basic setup code for enviornments.rs and mod.rs

---
+++ src/Biomes/mod.rs
@@ -0,0 +1,6 @@
+// This file is mostly to be an entry point for the enviornment module
+pub mod enviornment;
+pub mod noise;
+pub mod terrain;
+
+use crate::environment::environment::setup_environment;
