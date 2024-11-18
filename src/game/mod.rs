d77414837e3419680f505ce45c586bcc34d51e96
brant cass <brantcass7@gmail.com>
2024-11-17 21:03:12 -0600

    Removed errors when merging.

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,10 +1,7 @@
 mod biome;
-mod biome;
 mod systems;
 mod ui;
 
-<<<<<<< HEAD
-=======
 use crate::game::systems::pause_simulation;
 use crate::game::systems::resume_simulation;
 use crate::AppState;
@@ -27,13 +24,11 @@ impl Plugin for GamePlugin {
             // States
             .init_state::<SimulationState>()
             // On Enter Systems
-<<<<<<< HEAD
             .add_systems(OnEnter(AppState::Game), resume_simulation)
             // Plugins
             .add_plugins(GameUIPlugin)
             // Systems
             .add_systems(Update, toggle_simulation.run_if(in_state(AppState::Game)))
-=======
             .add_systems(OnEnter(AppState::Game), pause_simulation)
             // On Exit Systems
             .add_systems(OnExit(AppState::Game), pause_simulation);
--- src/game/systems.rs
+++ src/game/systems.rs
@@ -1,5 +1,4 @@
 use crate::game::biome::setup_terrain;
-use crate::game::biome::setup_terrain;
 use crate::game::SimulationState;
 use bevy::prelude::*;
 
