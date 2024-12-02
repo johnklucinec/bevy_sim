1944c76c6b261f3aa46f0acc5bcb4d1cc2115a56
Johnny <jsklucinec@gmail.com>
2024-12-02 17:39:27 -0500

    Added secondary camera view with a basic border outline

--- src/game/camera/components.rs
+++ src/game/camera/components.rs
@@ -2,3 +2,10 @@ use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct SecondaryCamera;
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum SecondaryCameraState {
+    #[default]
+    Hidden, // State for the main menu
+    Visible, // State for when the game is running
+}
---
+++ src/game/ui/camera_view/systems/layout.rs
@@ -0,0 +1,34 @@
+use crate::game::ui::camera_view::components::CameraViewUi;
+use crate::game::ui::camera_view::styles::*;
+use bevy::prelude::*;
+
+pub fn spawn_camera_view_ui(mut commands: Commands) {
+    build_camera_view(&mut commands);
+}
+
+pub fn despawn_camera_view_ui(
+    mut commands: Commands,
+    pause_menu_query: Query<Entity, With<CameraViewUi>>,
+) {
+    if let Ok(pause_menu_entity) = pause_menu_query.get_single() {
+        commands.entity(pause_menu_entity).despawn_recursive();
+    }
+}
+
+fn build_camera_view(commands: &mut Commands) -> Entity {
+    commands
+        .spawn((
+            camera_view_parent_style(),
+            // Camera Style
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                camera_view_style(),
+                BackgroundColor(BACKGROUND_COLOR),
+                BorderColor(Color::BLACK),
+                BorderRadius::all(Val::Px(10.0)),
+                CameraViewUi {},
+            ));
+        })
+        .id()
+}
