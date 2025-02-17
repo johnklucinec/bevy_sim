67c713c888f7cd5cd26ea77a09643d6134ecde2a
Roxamir <101136913+Roxamir@users.noreply.github.com>
2025-02-17 13:36:33 -0800

    create CarPlugin and remove car functions from main.rs

--- src/game/car/mod.rs
+++ src/game/car/mod.rs
@@ -1,3 +1,22 @@
+use bevy::prelude::*;
+
 pub mod car;
 pub mod physics;
 pub mod input;
+
+use crate::game::AppState;
+use crate::spawn_car;
+use crate::move_car;
+use crate::reset_car;
+
+pub struct CarPlugin;
+
+impl Plugin for CarPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            // Spawning the car
+            .add_systems(OnEnter(AppState::Game), spawn_car)
+            // Hide Camera View UI when exiting visible camera state
+            .add_systems(Update,(move_car, reset_car).run_if(in_state(AppState::Game)));
+    }
+}
