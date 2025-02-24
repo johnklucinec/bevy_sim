2d3da6dd5bfd014fec4b38ed213cf03aa3f957a6
Johnny <jsklucinec@gmail.com>
2025-02-24 14:24:42 -0500

    added the abilily to send custom commands.

---
+++ src/game/python/commands.rs
@@ -0,0 +1,31 @@
+use super::components::{CommandMessage, CommandQueue};
+use bevy::prelude::*;
+
+// Types of Commands that can be sent to the python script
+pub enum CommandType {
+    Detect,
+    Reset,
+    Speed,
+}
+
+// Implementation of the commands
+impl CommandType {
+    pub fn to_string(&self) -> &'static str {
+        match self {
+            CommandType::Detect => "DETECT",
+            CommandType::Reset => "RESET",
+            CommandType::Speed => "SPEED",
+        }
+    }
+}
+
+// Keyboard Commands
+pub fn queue_commands(mut commands: ResMut<CommandQueue>, input: Res<ButtonInput<KeyCode>>) {
+    if input.just_pressed(KeyCode::Space) {
+        commands.enqueue(CommandMessage::new(CommandType::Detect, "traffic cone"));
+    }
+
+    if input.just_pressed(KeyCode::KeyR) {
+        commands.enqueue(CommandMessage::new(CommandType::Reset, String::new()));
+    }
+}
