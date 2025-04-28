4fbbb3179d25e67569dc3b416540558b983ff74b
brant cass <brantcass7@gmail.com>
2025-04-27 21:39:00 -0500

    Getting a position of the car to update the chunks.

--- src/game/terrain/updatechunk.rs
+++ src/game/terrain/updatechunk.rs
@@ -2,6 +2,7 @@
 // and it will remove any that fall outside of a certain radius
 
 use crate::game::biome::roadspline::Spline;
+use crate::game::car::car::Car;
 use crate::game::terrain::chunk::{spawn_chunk, Chunk};
 use crate::game::terrain::noisewrapper::NoisePerlin;
 use crate::game::terrain::TerrainSettings;
@@ -9,21 +10,36 @@ use bevy::prelude::*;
 use noise::Perlin;
 use std::collections::HashSet;
 
-
 pub fn update_chunks(
     mut commands: Commands,
     chunk_query: Query<(Entity, &Chunk)>,
+    car_query: Query<&Transform, With<Car>>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     terrain_settings: Res<TerrainSettings>,
     perlin: Res<NoisePerlin>,
     road: Res<Spline>,
+    
 ) {
-    let fixed_position = Vec3::ZERO; // Or any other Vec3 you choose
+
+
+    let player_tf = car_query.single();
+
+    //getting cars position to determine which chunks to load
+    let car_transform = match car_query.get_single() {
+        Ok(t) => t,
+        Err(_) => {
+            // no car in world yet skip loading
+            return;
+        }
+    };
+    let player_pos = car_transform.translation;
+
+    // get the chunk coordinates based on the player's position
     let chunk_size = terrain_settings.chunk_size as f32;
     let reference_chunk_coord = IVec2::new(
-        (fixed_position.x / chunk_size).floor() as i32,
-        (fixed_position.z / chunk_size).floor() as i32,
+        (player_pos.x / chunk_size).floor() as i32,
+        (player_pos.z / chunk_size).floor() as i32,
     );
     // load radius (number of chunks on each side)
     let load_range = 2;
@@ -44,7 +60,9 @@ pub fn update_chunks(
     let loaded_coords: HashSet<IVec2> = chunk_query.iter().map(|(_, chunk)| chunk.coord).collect();
 
     for coord in desired_coords {
+        
         if !loaded_coords.contains(&coord) {
+            
             spawn_chunk(
                 &mut commands,
                 &mut meshes,
@@ -53,6 +71,7 @@ pub fn update_chunks(
                 &road,
                 &*perlin,
                 &terrain_settings,
+                player_tf,
             );
         }
     }
